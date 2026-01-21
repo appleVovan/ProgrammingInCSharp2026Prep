5933a53d2436c2a8d57507c04e30411bca69687e
applevovan <yablonskyi.v.reg@gmail.com>
2026-01-21 18:06:19 +0200

    Week2: Add initial code for intialization sample (class Student and Sample1 method)

--- Samples/Program.cs
+++ Samples/Program.cs
@@ -6,5 +6,18 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        #region Initialization in C# Examples
+        static void Sample1()
+        {
+            Student mystudent1 = new Student();
+            mystudent1.FirstName = "Steve";
+            mystudent1.LastName = "Jobs";
+
+            Student mystudent2 = new Student();
+            mystudent1.FirstName = "Bill";
+            mystudent1.LastName = "Gates";
+        }
+        #endregion
     }
 }
