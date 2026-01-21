3d2ed3326f13b7232a768ffe35228b0169142717
applevovan <yablonskyi.v.reg@gmail.com>
2026-01-21 18:37:34 +0200

    Week2: Copy Student file and class (compilation error)

---
+++ Samples/Student - Copy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KMA.ProgrammingInCSharp2026.Samples
+{
+    internal class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
