a196fc5ef43446c25e44f51cae287d112f64fd44
applevovan <yablonskyi.v.reg@gmail.com>
2026-01-21 19:00:17 +0200

    Week2: Add 3 different constructors to class Student

--- Samples/Student.cs
+++ Samples/Student.cs
@@ -13,5 +13,22 @@ namespace KMA.ProgrammingInCSharp2026.Samples
         {
             return FirstName + " " + LastName;
         }
+
+        public Student()
+        {
+            FirstName = "Default";
+            LastName = "Default";
+        }
+
+        public Student(string lastName)
+        {
+            LastName = LastName;
+        }
+
+        public Student(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = LastName;
+        }
     }
 }
