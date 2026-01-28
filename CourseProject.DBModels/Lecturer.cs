7bbc5edc05b401366128abe0662b09ee37d68bd5
applevovan <yablonskyi.v.reg@gmail.com>
2026-01-28 20:16:44 +0200

    Week3: Add Lecturer class

---
+++ CourseProject.DBModels/Lecturer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KMA.ProgrammingInCSharp2026.CourseProject.DBModels
+{
+    public enum LecturerPosition
+    {
+        Assistant,
+        Lecturer,
+        SeniorLecturer,
+        AssociateProfessor,
+        Professor,
+    }
+    public class Lecturer
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public LecturerPosition Position { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
