212fec3bf98ff6b3b8ef0151f9794e7d5aa733a2
applevovan <yablonskyi.v.reg@gmail.com>
2026-01-28 20:51:28 +0200

    Week3: DepartmentUIModel was refactored

--- CourseProject.UIModels/DepartmentUIModel.cs
+++ CourseProject.UIModels/DepartmentUIModel.cs
@@ -1,14 +1,48 @@
 ﻿using KMA.ProgrammingInCSharp2026.CourseProject.CourseProject.Common.Enums;
+using KMA.ProgrammingInCSharp2026.CourseProject.DBModels;
 
 namespace KMA.ProgrammingInCSharp2026.CourseProject.UIModels
 {    
 
     public class DepartmentUIModel  
     {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public Faculty Faculty { get; set; }
-        public List<LecturerUIModel> Lecturers { get; set; }
-        public int Staff { get; set; }
+        private DepartmentDBModel _dbModel;
+        private string _name;
+        private Faculty _faculty;
+        private List<LecturerUIModel> _lecturers;
+
+        public Guid? Id 
+        { 
+            get => _dbModel?.Id; 
+        }
+        public string Name 
+        { 
+            get => _name; 
+            set => _name = value; 
+        }
+        public Faculty Faculty 
+        { 
+            get => _faculty;
+            set => _faculty = value; 
+        }
+        public IReadOnlyList<LecturerUIModel> Lecturers 
+        { 
+            get => _lecturers; 
+        }
+        public int Staff 
+        { 
+            get => _lecturers?.Count ?? 0;
+        }
+
+        internal DepartmentUIModel()
+        {            
+        }
+
+        public DepartmentUIModel(DepartmentDBModel dBModel)
+        {
+            _dbModel = dBModel;
+            _name = dBModel.Name;
+            _faculty = dBModel.Faculty;            
+        }
     }
 }
