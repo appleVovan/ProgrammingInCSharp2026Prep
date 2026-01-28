ef60c587438d9f62bb7dfff32b72bc91d85345f7
applevovan <yablonskyi.v.reg@gmail.com>
2026-01-28 20:42:54 +0200

    Week3: Update modifiers and add constructors to DBModels

--- CourseProject.DBModels/DepartmentDBModel.cs
+++ CourseProject.DBModels/DepartmentDBModel.cs
@@ -2,12 +2,21 @@
 
 namespace KMA.ProgrammingInCSharp2026.CourseProject.DBModels
 {
-    
-
     public class DepartmentDBModel  
     {
-        public Guid Id { get; set; }
+        //Guid is generated only once during the creation of the object and cannot be changed later. 
+        public Guid Id { get; }        
         public string Name { get; set; }
         public Faculty Faculty { get; set; }
+
+        private DepartmentDBModel()
+        {            
+        }
+        public DepartmentDBModel(string name, Faculty faculty)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Faculty = faculty;
+        }
     }
 }
--- CourseProject.DBModels/LecturerDBModel.cs
+++ CourseProject.DBModels/LecturerDBModel.cs
@@ -7,11 +7,28 @@ namespace KMA.ProgrammingInCSharp2026.CourseProject.DBModels
 {    
     public class LecturerDBModel
     {
-        public Guid Id { get; set; }
-        public Guid DepartmentGuid { get; set; }
+        //Id is read-only to prevent modification after creation
+        public Guid Id { get; }
+        //Id of the department the lecturer cannot be changed after creation
+        public Guid DepartmentGuid { get; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public LecturerPosition Position { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        //DateOfBirth is set during creation and cannot be changed later
+        public DateTime DateOfBirth { get; }
+
+        private LecturerDBModel()
+        {
+        }
+
+        public LecturerDBModel(Guid departmentGuid, string firstName, string lastName, LecturerPosition position, DateTime dateOfBirth)
+        {
+            Id = Guid.NewGuid();
+            DepartmentGuid = departmentGuid;
+            FirstName = firstName;
+            LastName = lastName;
+            Position = position;
+            DateOfBirth = dateOfBirth;
+        }
     }
 }
