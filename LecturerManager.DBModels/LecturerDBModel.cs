100f7ad06234194bce2d10e7a95fbca00e1835de
applevovan <yablonskyi.v.reg@gmail.com>
2026-02-04 14:18:08 +0200

    Practice1: Add LecturerDBModel

---
+++ LecturerManager.DBModels/LecturerDBModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KMA.ProgrammingInCSharp2026.LecturerManager.DBModels
+{
+    public enum LecturerPosition
+    {
+        Assistant,
+        Lecturer,
+        SeniorLecturer,
+        AssociateProfessor,
+        Professor,
+    }
+    public class LecturerDBModel
+    {
+        public Guid Id { get; set; }
+        public Guid DepartmentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public LecturerPosition Position { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
