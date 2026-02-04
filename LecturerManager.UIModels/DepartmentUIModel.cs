65959aa79f74afcb18e6f503f59cd5e2108b4fb2
applevovan <yablonskyi.v.reg@gmail.com>
2026-02-04 14:41:06 +0200

    Practice1: Add initial UIModels

---
+++ LecturerManager.UIModels/DepartmentUIModel.cs
@@ -0,0 +1,14 @@
+﻿using KMA.ProgrammingInCSharp2026.LecturerManager.CommonComponents.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KMA.ProgrammingInCSharp2026.LecturerManager.UIModels
+{
+    public class DepartmentUIModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Faculty Faculty { get; set; }
+    }
+}
---
+++ LecturerManager.UIModels/LecturerUIModel.cs
@@ -0,0 +1,17 @@
+﻿using KMA.ProgrammingInCSharp2026.LecturerManager.CommonComponents.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KMA.ProgrammingInCSharp2026.LecturerManager.UIModels
+{
+    public class LecturerUIModel
+    {
+        public Guid Id { get; set; }
+        public Guid DepartmentGuid { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public LecturerPosition Position { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
