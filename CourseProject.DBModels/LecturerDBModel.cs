6de07ba2843c1a20c653ec3fc1587231adc6023c
applevovan <yablonskyi.v.reg@gmail.com>
2026-02-04 13:10:49 +0200

    Revert "Week3: Add UIModels, add DepartmentId to Lecturer"
    This reverts commit 3c7d1e5dab0a85963ef3082cac0d68fff400c86f.

--- CourseProject.DBModels/LecturerDBModel.cs
+++ CourseProject.DBModels/LecturerDBModel.cs
@@ -8,7 +8,6 @@ namespace KMA.ProgrammingInCSharp2026.CourseProject.DBModels
     public class LecturerDBModel
     {
         public Guid Id { get; set; }
-        public Guid DepartmentGuid { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public LecturerPosition Position { get; set; }
