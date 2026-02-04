df9332169c69f9df0b2f0d74916e476aaf659be8
applevovan <yablonskyi.v.reg@gmail.com>
2026-02-04 15:04:18 +0200

    Practice1: Link Lecturer UI and DB models

--- LecturerManager.UIModels/LecturerUIModel.cs
+++ LecturerManager.UIModels/LecturerUIModel.cs
@@ -1,4 +1,5 @@
 ﻿using KMA.ProgrammingInCSharp2026.LecturerManager.CommonComponents.Enums;
+using KMA.ProgrammingInCSharp2026.LecturerManager.DBModels;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,12 +8,75 @@ namespace KMA.ProgrammingInCSharp2026.LecturerManager.UIModels
 {
     public class LecturerUIModel
     {
-        public Guid Id { get; set; }
-        public Guid DepartmentGuid { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public LecturerPosition Position { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public int Age { get; set; }
+        private LecturerDBModel _dbModel;
+        private Guid _departmentId;
+        private string _firstName;
+        private string _lastName;
+        private LecturerPosition _position;
+        private DateTime _dateOfBirth;
+        private int _age;
+
+        public Guid? Id
+        {
+            get => _dbModel?.Id;
+        }
+        public Guid DepartmentId
+        {
+            get => _departmentId;
+        }
+        public string FirstName
+        {
+            get => _firstName;
+            set => _firstName = value;
+        }
+        public string LastName
+        {
+            get => _lastName;
+            set => _lastName = value;
+        }
+        public LecturerPosition Position
+        {
+            get => _position;
+            set => _position = value;
+        }
+        public DateTime DateOfBirth
+        {
+            get => _dateOfBirth;
+            set
+            {
+                if (_dbModel != null)
+                    return; //Prevent changing DateOfBirth if DBModel exists
+                _dateOfBirth = value;
+                CalculateAge();
+            }
+        }
+        public int Age
+        {
+            get => _age;
+        }
+        // Constructor used to create new Lecturer
+        public LecturerUIModel(Guid departmentId)
+        {
+            _departmentId = departmentId;
+        }
+        // Constructor used to load existing Department for viewing/editing
+        public LecturerUIModel(LecturerDBModel dBModel)
+        {
+            _dbModel = dBModel;
+            _departmentId = dBModel.DepartmentId;
+            _firstName = dBModel.FirstName;
+            _lastName = dBModel.LastName;
+            _position = dBModel.Position;
+            _dateOfBirth = dBModel.DateOfBirth;
+            CalculateAge();
+        }
+
+        private void CalculateAge()
+        {
+            var today = DateTime.Today;
+            _age = today.Year - _dateOfBirth.Year;
+            if (_dateOfBirth.Date > today.AddYears(-_age))
+                _age--;
+        }
     }
 }
