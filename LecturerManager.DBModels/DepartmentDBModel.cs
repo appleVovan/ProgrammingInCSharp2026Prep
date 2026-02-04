f9a7d54782a0ce1ff975411421cdc9772ccb859a
applevovan <yablonskyi.v.reg@gmail.com>
2026-02-04 13:32:04 +0200

    Practice1: Add DepartmentDB Model

---
+++ LecturerManager.DBModels/DepartmentDBModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KMA.ProgrammingInCSharp2026.LecturerManager.DBModels
+{
+    public enum Faculty
+    {
+        FacultyOfInformatics,
+        FacultyOfEconomics,
+        FacultyOfLaw,
+        FacultyOfNaturalSciences
+    }
+
+    public class DepartmentDBModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Faculty Faculty { get; set; }
+    }
+}
