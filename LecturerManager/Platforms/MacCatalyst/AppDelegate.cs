407f28d00ceef8bc8e078b39f905d4ac3fcacd3b
applevovan <yablonskyi.v.reg@gmail.com>
2026-02-11 17:53:42 +0200

    Practice 2: Add initial MAUI project

---
+++ LecturerManager/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LecturerManager
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
