ae5b6f4efefa06556fe13580c6057d21cde4bda1
applevovan <yablonskyi.v.reg@gmail.com>
2026-02-15 13:11:55 +0200

    Practice2: Remove samples

--- LecturerManager/Pages/LecturerCreatePage.xaml.cs
+++ LecturerManager/Pages/LecturerCreatePage.xaml.cs
@@ -5,30 +5,5 @@ public partial class LecturerCreatePage : ContentPage
 	public LecturerCreatePage()
 	{
 		InitializeComponent();
-	}
-
-    private void Button_Clicked(object sender, EventArgs e)
-    {
-        var currentBut = (Button)sender;
-		currentBut.Text += "Ha";
-        if (currentBut.Text.StartsWith("Button 2"))
-		{
-            var newBut = new Button();
-            newBut.Text = "Button 4";
-            newBut.Clicked += Button_Clicked;
-            Grid grid = ((Grid)Content);
-            grid.Children.Add(newBut);
-            grid.SetRow(newBut, 1);
-            grid.SetColumn(newBut, 1);
-        } else if (currentBut.Text.StartsWith("Button 4"))
-        {
-            var newBut = new Button();
-            newBut.Text = "Button 1";
-            newBut.Clicked += Button_Clicked;
-            Grid grid = ((Grid)Content);
-            grid.Children.Add(newBut);
-            grid.SetRow(newBut, 0);
-            grid.SetColumn(newBut, 0);
-        }
-    }
+	}    
 }
