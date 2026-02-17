6a7102609604be5d99cff21a0e2200d19c656154
applevovan <yablonskyi.v.reg@gmail.com>
2026-02-17 19:23:24 +0200

    Practice 2: Add Departmentd Loading

--- LecturerManager/Pages/DepartmentsPage.xaml.cs
+++ LecturerManager/Pages/DepartmentsPage.xaml.cs
@@ -1,9 +1,22 @@
+using KMA.ProgrammingInCSharp2026.LecturerManager.Services;
+using KMA.ProgrammingInCSharp2026.LecturerManager.UIModels;
+using System.Collections.ObjectModel;
+
 namespace KMA.ProgrammingInCSharp2026.LecturerManager.Pages;
 
 public partial class DepartmentsPage : ContentPage
 {
+	private StorageService _storage;
+	public ObservableCollection<DepartmentUIModel> Departments { get; set; }
 	public DepartmentsPage()
 	{
 		InitializeComponent();
+		_storage = new StorageService();
+		Departments = new ObservableCollection<DepartmentUIModel>();
+		foreach (var department in _storage.GetAllDepartments())
+		{
+			Departments.Add(new DepartmentUIModel(department));
+		}
+		BindingContext = this;
 	}
 }
