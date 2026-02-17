63a232ca6ba5e946e48d9c48bee2a4c44b8e3aad
applevovan <yablonskyi.v.reg@gmail.com>
2026-02-17 21:12:25 +0200

    Practice 2: Add Enum Converter

--- LecturerManager.CommonComponents/Enums/Faculty.cs
+++ LecturerManager.CommonComponents/Enums/Faculty.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace KMA.ProgrammingInCSharp2026.LecturerManager.CommonComponents.Enums
 {
     public enum Faculty
     {
+        [Display(Name = "Faculty of Informatics")]
         FacultyOfInformatics,
+        [Display(Name = "Faculty of Economics")]
         FacultyOfEconomics,
+        [Display(Name = "Faculty of Law")]
         FacultyOfLaw,
+        [Display(Name = "Faculty of Natural Sciences")]
         FacultyOfNaturalSciences
     }
 }
---
+++ LecturerManager/Tools/Converters/EnumToDisplayNameConverter.cs
@@ -0,0 +1,27 @@
+﻿using KMA.ProgrammingInCSharp2026.LecturerManager.CommonComponents;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Reflection;
+using System.Text;
+
+namespace KMA.ProgrammingInCSharp2026.LecturerManager.Tools.Converters
+{
+    public class EnumToDisplayNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null) 
+                return string.Empty;
+            
+            if (value is not Enum castedEnum) 
+                return value.ToString() ?? string.Empty;
+
+            return castedEnum.GetDisplayName();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotSupportedException();
+    }
+}
