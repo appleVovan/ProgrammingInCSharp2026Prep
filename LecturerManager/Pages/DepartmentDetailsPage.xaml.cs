ee61d1f8e4f76ff9c39c647759b2d4b0387c9d30
applevovan <yablonskyi.v.reg@gmail.com>
2026-02-18 11:54:38 +0200

    Practice 2: Add department details to DepartmentDetailsPage

--- LecturerManager/Pages/DepartmentDetailsPage.xaml.cs
+++ LecturerManager/Pages/DepartmentDetailsPage.xaml.cs
@@ -1,9 +1,17 @@
+using KMA.ProgrammingInCSharp2026.LecturerManager.Services;
+using KMA.ProgrammingInCSharp2026.LecturerManager.UIModels;
+
 namespace KMA.ProgrammingInCSharp2026.LecturerManager.Pages;
 
 public partial class DepartmentDetailsPage : ContentPage
 {
-	public DepartmentDetailsPage()
+    public DepartmentUIModel CurrentDepartment { get; set; }
+    public DepartmentDetailsPage()
 	{
 		InitializeComponent();
-	}
+		var storage = new StorageService();
+		var department = storage.GetAllDepartments().FirstOrDefault();
+		CurrentDepartment = new DepartmentUIModel(department);
+		BindingContext = CurrentDepartment;
+    }
 }
