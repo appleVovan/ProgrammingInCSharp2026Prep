8a6947cd0c27e35123c2382550377f585af08067
applevovan <yablonskyi.v.reg@gmail.com>
2026-02-18 11:10:52 +0200

    Practice 2: Add DepartmentDetailsPage

---
+++ LecturerManager/Pages/DepartmentDetailsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace KMA.ProgrammingInCSharp2026.LecturerManager.Pages;
+
+public partial class DepartmentDetailsPage : ContentPage
+{
+	public DepartmentDetailsPage()
+	{
+		InitializeComponent();
+	}
+}
