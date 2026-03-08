c47aab59d803c30b6a53689256a8b0fbb0b69981
applevovan <yablonskyi.v.reg@gmail.com>
2026-03-08 14:25:27 +0200

    Practice 3: Add IDepratmentService

---
+++ LecturerManager.Services/IDepartmentService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KMA.ProgrammingInCSharp2026.LecturerManager.Services
+{
+    internal interface IDepartmentService
+    {
+        public IEnumerable<> GetAllDepartments();
+    }
+}
