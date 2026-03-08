c10b4ab1ccc4bafba945b199736178505b8dbec0
applevovan <yablonskyi.v.reg@gmail.com>
2026-03-08 14:31:24 +0200

    Practice 3: Add DepartmentListDTO

---
+++ LecturerManager.DTOModels/Depratment/DepartmentListDTO.cs
@@ -0,0 +1,23 @@
+﻿using KMA.ProgrammingInCSharp2026.LecturerManager.CommonComponents.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KMA.ProgrammingInCSharp2026.LecturerManager.DTOModels.Depratment
+{
+    public class DepartmentListDTO
+    {
+        public Guid Id { get; }
+        public string Name { get; }
+        public Faculty Faculty { get; }
+        public int LecturersCount { get; set; }
+
+        public DepartmentListDTO(Guid id, string name, Faculty faculty, int lecturersCount)
+        {
+            Id = id;
+            Name = name;
+            Faculty = faculty;
+            LecturersCount = lecturersCount;
+        }
+    }
+}
