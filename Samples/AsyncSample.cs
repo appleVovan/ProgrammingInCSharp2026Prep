65393ae50b9e64b56410ee5b6cdff6f1398212b7
applevovan <yablonskyi.v.reg@gmail.com>
2026-03-18 12:15:34 +0200

    Week 10: no exceptions

--- Samples/AsyncSample.cs
+++ Samples/AsyncSample.cs
@@ -36,16 +36,7 @@ namespace KMA.ProgrammingInCSharp2026.Samples
             Console.WriteLine($"3 Step. Thread: {Thread.CurrentThread.ManagedThreadId}");
             var task = RunLongOperationAsync();
             Console.WriteLine($"9 Step. Thread: {Thread.CurrentThread.ManagedThreadId}");
-            //try
-            //{
-                Console.WriteLine($"Result is {await task}");
-            throw new Exception("Test exception");
-            //}
-            //catch (Exception ex)
-            //{
-            //   Console.WriteLine($"{ex.Message}.Thread: { Thread.CurrentThread.ManagedThreadId}");
-            //    throw;
-            //}
+            Console.WriteLine($"Result is {await task}");            
             Console.WriteLine($"10 Step. Thread: {Thread.CurrentThread.ManagedThreadId}");
         }
     }
